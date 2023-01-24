807932fe764b32f26d315ca1d622a2c95c0e464d
EdwinVega123 <122801517+EdwinVega123@users.noreply.github.com>
2023-01-24 14:06:53 +0000

    se realizo el programa 2 para encontrar errores comunes

---
+++ semana2/programa2.py
@@ -0,0 +1,10 @@
+"""
+    Programa2
+    Nombre: Edwin MVP
+    Fecha: 24/01/2023
+    Descripción: encontrar los errores para poder hacer bien el programa, biendo correctamente la concatenacion, llaves, poner bien puntos y comas etc
+"""
+variable1 = " Hola " # variable para almacenar una cadena de caracteres
+variable2 = 619 # variable para almacenar una cadena de caracteres
+
+print(" {} {} ".format(variable1 , variable2)) 
