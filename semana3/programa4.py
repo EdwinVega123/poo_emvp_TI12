b724b17287f869ab71822ddf43b2fe1b8e81784b
EdwinVega123 <122801517+EdwinVega123@users.noreply.github.com>
2023-01-30 14:56:17 +0000

    ejecutamos el programa 4 para suma, resta, multiplicación, división y potencia

---
+++ semana3/programa4.py
@@ -0,0 +1,19 @@
+"""
+    Programa3
+    Nombre: Edwin MVP
+    Fecha: 24/01/2023
+    Descripción:Cambiar el tipo de dato
+"""
+numero1 = input("Numero1: ")#variable para imprimir
+numero2 = 10#variable de tipo entero
+
+suma = int(numero1) + numero2#suma de dos numeros
+resta = int(numero1) - numero2#resta de dos numeros
+multi = int(numero1) * numero2#multiplicacion de dos numeros
+div = int(numero1) / numero2#division de dos numeros
+pote = int(numero1) ** numero2 
+print(suma)
+print(resta)
+print(multi)
+print(div)
+print(pote)
