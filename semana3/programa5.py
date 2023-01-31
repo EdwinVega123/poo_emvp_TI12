3940ed9f882bc68fe9947b2f4dbc93a7bfa710b0
EdwinVega123 <122801517+EdwinVega123@users.noreply.github.com>
2023-01-31 14:09:20 +0000

    se ha corregido el programa 5 en las variables

--- semana3/programa5.py
+++ semana3/programa5.py
@@ -4,15 +4,14 @@
     Fecha: 31/01/2023
     Descripción:Area y Permietro de un triangulo
 """
-A = input("Ingrese el primer lado: ")
-B = input("Ingrese el segundo lado: ")
-C = input("Ingrese el tercer lado: ")
+lado_a = int(input("Ingrese el primer lado: "))#variable para imprimir o almacenar un numero
+lado_b = int(input("Ingrese el segundo lado: "))#variable para imprimir  o almacenar un numero
+lado_c = int(input("Ingrese el tercer lado: "))#variable para imprimir o almacenar  otro numero
+altura = int(input("Ingrese la altura: "))#almacena una cadena de caracteres
+base = int(input("Ingrese la base: "))#almacena una cadena de caracteres 
 
-Altura = int(input("Ingrese la altura: "))
-Base = int(input("Ingrese la base:"))
+perimetro = lado_a+ lado_b + lado_c
+area = int (base * altura / 2)
 
-Perimetro = (A) + (B) + (C)
-Area = Base * Altura / 2
-
-print(Perimetro)
-print(Area)
+print("El perimetro es", perimetro)
+print("El area es", area)
---
+++ semana3/programa6.py
@@ -0,0 +1,6 @@
+"""
+    Programa3
+    Nombre: Edwin MVP
+    Fecha: 31/01/2023
+    Descripción:Area y Permietro de un Circulo
+"""
