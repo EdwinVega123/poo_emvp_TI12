6c91f53b0ca3cf3379f9cde055b73e8cfad7da10
EdwinVega123 <122801517+EdwinVega123@users.noreply.github.com>
2023-01-31 15:00:20 +0000

    se hizo el programa 6

--- semana3/programa6.py
+++ semana3/programa6.py
@@ -4,3 +4,11 @@
     Fecha: 31/01/2023
     Descripción:Area y Permietro de un Circulo
 """
+radio = int(input("Ingrese el radio del circulo: "))#variable para imprimir  o almacenar un numero
+pi = 3.14 #variable de tipo entero
+
+perimetro_circulo = 2*pi*radio
+area_circulo = pi*radio*radio
+
+print("El perimetro del circulo con un radio de {} es {} ".format(radio, perimetro_circulo))
+print("El area del circulo con un radio de {} es {} ".format(radio, area_circulo))
---
+++ semana3/programa7.py
@@ -0,0 +1,14 @@
+"""
+    Programa3
+    Nombre: Edwin MVP
+    Fecha: 31/01/2023
+    Descripción:Area y Permietro de un Cuadrado
+"""
+lado_circulo = int(input("Ingrese el radio del circulo: "))#variable para imprimir  o almacenar un numero
+
+
+area = lado_circulo * lado_circulo
+perimetro = lado_circulo + lado_circulo + lado_circulo + lado_circulo
+
+print("el area es", area)
+print("el perimetro es", perimetro)
