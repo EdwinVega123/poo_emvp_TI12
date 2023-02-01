8fa37cb9ec8d0302c76a0ef04de0c32199d4f28c
EdwinVega123 <122801517+EdwinVega123@users.noreply.github.com>
2023-02-01 14:54:27 +0000

    se ejecuto el programa 8 para comparar dos numeros y nos de el mayor

--- semana3/programa7.py
+++ semana3/programa7.py
@@ -4,11 +4,11 @@
     Fecha: 31/01/2023
     Descripción:Area y Permietro de un Cuadrado
 """
-lado_circulo = int(input("Ingrese el radio del circulo: "))#variable para imprimir  o almacenar un numero
+lado_cuadrado = int(input("Ingrese el lado del circulo: "))#variable para imprimir  o almacenar un numero
 
 
-area = lado_circulo * lado_circulo
-perimetro = lado_circulo + lado_circulo + lado_circulo + lado_circulo
+area = lado_cuadrado * lado_cuadrado
+perimetro = lado_cuadrado + lado_cuadrado + lado_cuadrado + lado_cuadrado
 
 print("el area es", area)
 print("el perimetro es", perimetro)
---
+++ semana3/programa8.py
@@ -0,0 +1,15 @@
+"""
+    Programa8
+    Nombre: Edwin MVP
+    Fecha: 1/01/2023
+    Descripción:Comparar 2 numeros enteros e imprimir el numero mayor
+"""
+numero1 = int(input("Dame el primer numero: "))#variable para imprimir
+numero2 = int(input("Dame el segundo numero: "))#variable para imprimir
+
+if      numero1 > numero2:
+    print ("el mayor es", numero1)
+  
+else: 
+    print ("el mayor es", numero2)
+
